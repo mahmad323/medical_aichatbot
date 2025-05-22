1fa609ecbecb6a5c04085aa24ddf79b4948abbae
mahmad323 <muhammadahmadme085@gmail.com>
2025-05-22 17:22:51 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Muhammad Ahmad',
+    author_email= 'muhammadahmadme085@gmail.com@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
